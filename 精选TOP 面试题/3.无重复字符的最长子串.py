6799ae35b511cf198425da771a44bcc06fe7e6a4
e <1069679911@qq.com>
2019-07-18 08:25:42 +0800

    J

---
+++ 精选TOP 面试题/3.无重复字符的最长子串.py
@@ -0,0 +1,44 @@
+# class Solution:
+#     def lengthOfLongestSubstring(self, s):
+#         if len(s)==0:
+#             return 0
+#         start = s[0]
+#         res = []
+#         for i in range(len(s)-1):
+#             if (s[i+1] != s[i]) and (s[i+1] not in start):
+#                 start += s[i]
+#                 if ((i + 1) == len(s) - 1) or (i==0):
+#                     start += s[i + 1]
+#                 else:
+#                     start += s[i]
+#             else:
+#                 res.append(len(start))
+#                 start = s[i]
+#         res.append(len(start))
+#         if len(res)==0:
+#             return 1
+#         return max(res)
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        if len(s) <= 1:
+            return len(s)
+        start = ''
+        res = []
+        for j in range(len(s)):
+            if s[j] not in start:
+                start += s[j]
+            else:
+                temp = start.index(s[j])
+                res.append(len(start))
+                start = start[temp+1:]+s[j]
+                if j == len(s) - 1:
+                    return max(res)
+            if (start!=''):
+                res.append(len(start))
+            if j==len(s)-1:
+                return max(res)
+
+string = "au"
+s = Solution()
+r = s.lengthOfLongestSubstring(string)
+print(r)
